841a81f52f6a8118de1089ca6e254f246f6558fe
aytekin <mjeong827@gmail.com>
2021-10-31 22:32:29 +0900

    21.10.31

--- secretary_app/project.py
+++ secretary_app/project.py
@@ -27,35 +27,54 @@ def scrape_weather():
     weather_summary = soup.find("p",attrs={"class":"summary"}).find("span",attrs={"class":"weather before_slash"}).get_text()
     print(weather_summary,", 어제보다",weather_degree)
 
+    summary = weather_summary + ", 어제보다" + weather_degree.strip()
+
+    res = [temperature, summary]
+
     # 미세먼지, 초미세먼지, 자외선, 일몰
     chart_list = soup.find("ul",attrs={"class":"today_chart_list"}).find_all('li')
+    chart_res = []
     for attrs in chart_list:
         print(attrs.get_text().strip())
+        res.append(attrs.get_text().strip())
     print()
+    
+    return res
+
 
 def scrape_news():
     url = 'https://news.naver.com'
     soup = create_soup(url)
     print('[헤드라인 뉴스]')
     hdline_article_list = soup.find("ul",attrs={"class":"hdline_article_list"}).find_all("li",limit=3)
+    res = []
     for index,li in enumerate(hdline_article_list):
         headline = li.find("a").get_text().strip()
         link = url + li.find("a")['href']
         print(f'{index+1}. {headline}')
         print(f'(링크 : {link})')
+        res.append(f'{index+1}. {headline}')
+        res.append(f'(링크 : {link})')
     print()
+
+    return res
 
 def scrape_IT_news():
     url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105'
     soup = create_soup(url)
     print('[IT 뉴스]')
     news_list = soup.find_all("div",attrs={"class":"cluster_group _cluster_content"},limit=3) # 3개까지만 가져오기
+    res = []
     for index,li in enumerate(news_list):
         title = li.find("a",attrs={"class":"cluster_text_headline nclicks(cls_sci.clsart)"}).get_text()
         link = li.find("a",attrs={"class":"cluster_text_headline nclicks(cls_sci.clsart)"})['href']
         print(f'{index+1}. {title}')
         print(f'(링크 : {link}')
+        res.append(f'{index+1}. {title}')
+        res.append(f'(링크 : {link}')
     print()
+
+    return res
 
 def eng_today_conv():
     url = 'https://www.hackers.co.kr/?c=s_eng/eng_contents/I_others_english'
@@ -67,15 +86,20 @@ def eng_today_conv():
     # 영어 지문 scraping
     conv_text = soup.find_all('div',attrs={"class":"conv_txt"})
     print('(영어 지문)')
+    eng_sentences = []
     for sentence in conv_text[1].find_all("div"):
         print(sentence.get_text().strip())
+        eng_sentences.append(sentence.get_text().strip())
 
     print()
     # 한글지문 scraping
     print('(한글 지문)')
+    kor_sentences = []
     for sentence in conv_text[0].find_all("div"):
-        print(sentence.get_text().strip())    
+        print(sentence.get_text().strip())
+        kor_sentences.append(sentence.get_text().strip())
 
+    return today_sentence, eng_sentences, kor_sentences
     
 if __name__ == "__main__":
     scrape_weather() # 오늘의 날씨 정보 가져오기
--- secretary_app/secretary_gui.py
+++ secretary_app/secretary_gui.py
@@ -10,10 +10,41 @@ todayinfo_frame = LabelFrame(root,text='비서버튼')
 todayinfo_frame.pack(padx=5,pady=5, ipady=5, ipadx=5)
 
 def secretary():
-    scrape_weather()
-    scrape_news()
-    scrape_IT_news()
-    eng_today_conv()
+    text.delete('1.0',END)
+    text.insert(END,'[오늘의 날씨]\n')
+    for i in scrape_weather():
+        text.insert(END,i)
+        text.insert(END,'\n')
+    text.insert(END,'\n')
+
+    text.insert(END, '[헤드라인 뉴스]\n')
+    for i in scrape_news():
+        text.insert(END,i)
+        text.insert(END,'\n')
+    text.insert(END,'\n')
+
+    text.insert(END, '[IT 뉴스]\n')
+    for i in scrape_news():
+        text.insert(END,i)
+        text.insert(END,'\n')
+    text.insert(END,'\n')
+
+    text.insert(END, '[오늘의 영어 회화]\n')
+    eng_today = eng_today_conv()
+    text.insert(END,eng_today[0])
+    text.insert(END, '\n(영어 지문)\n')
+
+    for i in eng_today[1]:
+        text.insert(END,i)
+        text.insert(END,'\n')
+    text.insert(END,'\n')
+
+    text.insert(END, '\n(한글 지문)\n')
+
+    for i in eng_today[2]:
+        text.insert(END,i)
+        text.insert(END,'\n')
+    text.insert(END,'\n')
 
 btn_info = Button(todayinfo_frame,text='호출',command=secretary)
 btn_info.pack()
